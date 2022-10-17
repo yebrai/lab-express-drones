8a9d105f90b26b54b67352aa69e359a835f4b070
yebrai <ivansevv@gmail.com>
2022-10-17 21:25:38 +0200

    done with async await + then

--- routes/drones.js
+++ routes/drones.js
@@ -1,75 +1,120 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Drone = require("../models/Drone.model.js")
+const Drone = require("../models/Drone.model.js");
 
 // require the Drone model here
 
-router.get('/drones', (req, res, next) => {
+router.get("/drones", (req, res, next) => {
   // Iteration #2: List the drones
   // ... your code here
- Drone.find()
- .then((response)=>{
-    res.render("drones/list.hbs", {
-      response
+  Drone.find()
+    .then((response) => {
+      res.render("drones/list.hbs", {
+        response,
+      });
     })
- })
- .catch((error)=>{
-  next(error)
- })
-
+    .catch((error) => {
+      next(error);
+    });
 });
 
-router.get('/drones/create', (req, res, next) => {
-  
-  res.render("drones/create-form.hbs")
-
+router.get("/drones/create", (req, res, next) => {
+  res.render("drones/create-form.hbs");
 });
 
-router.post('/drones/create', (req, res, next) => {
-  
+//con Then
+// router.post('/drones/create', (req, res, next) => {
 
+//   Drone.create(req.body)
+//   .then((response) => {
+
+//     res.redirect("/drones")
+//   })
+//   .catch((err) => {
+//     next(err)
+//   })
+
+// });
+
+// con async await
+router.post("/drones/create", async (req, res, next) => {
+try {
+  console.log("creando drone")
   Drone.create(req.body)
-  .then((response) => {
-    
-    res.redirect("/drones")
-  })
-  .catch((err) => {
+  await res.redirect("/drones")
+}
+catch (err) {
+next(err)
+}
+});
+
+// router.get("/drones/:id/edit", (req, res, next) => {
+//   const { id } = req.params;
+//   Drone.findById(id).then((response) => {
+//     res.render("drones/update-form.hbs", {
+//       response,
+//     });
+//   });
+// });
+
+//con async await
+router.get("/drones/:id/edit", async (req, res, next) => {
+  const { id } = req.params;
+  try {
+      let response = await Drone.findById(id)
+      await res.render("drones/update-form.hbs", {response})
+
+  }
+  catch(err){
     next(err)
-  })
-
-
+    }
 });
 
-router.get('/drones/:id/edit', (req, res, next) => {
-  const {id} = req.params
-  Drone.findById(id)
-  .then((response)=>{
-    res.render("drones/update-form.hbs", {
-      response
-  })
-    
-  })
+//con Then
+// router.post("/drones/:id/edit", (req, res, next) => {
+//   const { id } = req.params;
+//   console.log("Patata");
+//   Drone.findByIdAndUpdate(id, req.body).then((response) => {
+//     console.log("POtotO");
+//     res.redirect("/drones");
+//   });
+// });
+
+//con Async Await
+router.post("/drones/:id/edit", async (req, res, next) => {
+  const { id } = req.params;
+  try {
+   await Drone.findByIdAndUpdate(id, req.body)
+    console.log(req.body);
+   await res.redirect("/drones");
+  }
+  catch (err) {
+    next(err)
+    }
 });
 
-router.post('/drones/:id/edit', (req, res, next) => {
-  const {id} = req.params
-  console.log("Patata")
-  Drone.findByIdAndUpdate(id, req.body)
-  .then((response)=>{
-    console.log("POtotO")
-    res.redirect("/drones")
-  })
-});
+//con Then
+// router.post("/drones/:id/delete", (req, res, next) => {
+//   const { id } = req.params;
 
-router.post('/drones/:id/delete', (req, res, next) => {
+//   Drone.findByIdAndDelete(id).then((response) => {
+//     res.redirect("/drones");
+//     console.log();
+//   });
+// });
+
+//Con Asyn Await
+router.post("/drones/:id/delete", async (req, res, next) => {
+  const { id } = req.params;
+try {
+  await Drone.findByIdAndDelete(id)
+    res.redirect("/drones");
+    console.log();
+}
+catch (err) {
+  next(err)
+  }
   
-  const { id } = req.params
-
-  Drone.findByIdAndDelete(id)
-  .then((response) => {
-    res.redirect("/drones")
-    console.log()
-  })
 });
 
 module.exports = router;
